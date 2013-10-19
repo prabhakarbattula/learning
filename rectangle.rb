60c80e861ab30fa0a166ef78d3e706be004a3785
Prabakar Battula <prabhakar.battula@gmail.com>
2013-10-19 11:54:12 +0530

    Delete rectangle.rb

--- rectangle.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby-local-exe
-puts "Length of Rectangle in centimeter"
-length = gets.to_i
-puts "Breadth of Rectange in centimeter"
-breadth = gets.to_i
-a=length+breadth
-perimeter = 2*a
-area = length * breadth
-puts "Area of Rectangle is #{area} square centimeter and Perimeter is #{perimeter} centimeter."
