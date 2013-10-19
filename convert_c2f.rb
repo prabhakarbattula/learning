355daa82178774390d13fac0421417d5ebda9add
Prabakar Battula <prabhakar.battula@gmail.com>
2013-10-19 11:53:00 +0530

    Delete convert_c2f.rb

--- convert_c2f.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby-local-exe
-puts "Enter the Celsius value to convert"
-celsius = gets.to_i
-farenheit = 9/5*(celsius+32)
-puts "#{celsius} degree centigrade is equal to #{farenheit} degree farenheit."
